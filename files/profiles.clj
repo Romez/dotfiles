5a2ef417549fd2b3184adde1134b860fb8afa0f9
roman_ushakov <roman.ushakov.89@inbox.ru>
2022-06-08 13:43:49 +0200

    fix nrepl lein

--- files/profiles.clj
+++ files/profiles.clj
@@ -1,4 +1,8 @@
 {:user {:plugins [[lein-ancient "0.7.0"]
                   [lein-pprint "1.3.2"]]}
- :repl {:global-vars {*print-length* 100}}}
+ :repl {:global-vars {*print-length* 100}
+        :middleware [cider-nrepl.plugin/middleware
+                     refactor-nrepl.plugin/middleware]
+        :plugins [[cider/cider-nrepl "RELEASE"]
+                  [refactor-nrepl/refactor-nrepl "RELEASE"]]}}
 
