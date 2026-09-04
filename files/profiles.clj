6498eda69115ed56aca7c300fad60814a831cf22
roman_ushakov <roman.ushakov.899@gmail.com>
2026-09-04 10:56:59 +0300

    rm clojure profiles.clj

--- files/profiles.clj
+++
@@ -1,8 +0,0 @@
-{:user {:plugins [[lein-ancient "0.7.0"]
-                  [lein-pprint "1.3.2"]]}
- :repl {:global-vars {*print-length* 100}
-        :middleware [cider-nrepl.plugin/middleware
-                     refactor-nrepl.plugin/middleware]
-        :plugins [[cider/cider-nrepl "RELEASE"]
-                  [refactor-nrepl/refactor-nrepl "RELEASE"]]}}
-
